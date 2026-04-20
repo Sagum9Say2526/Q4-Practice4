351681e14161636d841fc1ad85f4cfb352c008d2
Sagum9Say2526 <sagum.pa@obmontessori.edu.ph>
2026-04-21 07:36:11 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function garageGate () {
+    let carCounter = 18;
+    const maxCars = 25;
+
+    while (carCounter < maxCars) {
+        window.alert("Car No. " + carCounter + " is entering");
+    }
+}
